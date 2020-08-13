320a7721a1308b235e12a3a016e91cf9634d5cbf
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-08-13 13:04:23 +0430

    install.sh updating added

--- install.sh
+++ install.sh
@@ -15,7 +15,13 @@ else
 	echo -e "[\033[0;31m-\033[0m] Install 'python3' on your distro"
 fi
 
-sudo mkdir /usr/share/netget
+echo -n "Are you installing netget for the first time? [Y,n]> "
+
+read answer
+
+if [[ "$answer" != "N" && "$answer" != 'n' ]];then
+	sudo mkdir /usr/share/netget
+fi
 
 sudo cp bin/netget /bin/
 

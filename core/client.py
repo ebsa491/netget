820375cdb746ea0cd1ffdbd137fdf0a07ef8039f
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-08-20 11:44:46 +0430

    Descriptions added (client.py)

--- core/client.py
+++ core/client.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 # License : GPL v3.0
 # Created by Saman Ebrahimnezhad .
+#
+# This script will connect to the server and will save the downloaded file.
+# You can use this script personally but pay attention to the license file.
 
 import socket
 import signal
@@ -8,9 +11,11 @@ import sys
 import os
 
 class NetgetClient:
+	"""This is the main class of the client program."""
+
 	def __init__(self):
 
-		if len(sys.argv) > 2 and str(sys.argv[1]) != __file__:
+		if len(sys.argv) > 2 and str(sys.argv[1]) != __file__: # this condition checks the script's arguments.
 
 			try:
 
@@ -21,7 +26,7 @@ class NetgetClient:
 				print("[\033[0;31m-\033[0m] Error in connecting to the server...")
 				sys.exit(1)
 
-			signal.signal(signal.SIGINT, self.ctrlC)
+			signal.signal(signal.SIGINT, self.ctrlC) # if user sends SIGINT (same as ^C) signal ctrlC method will be called.
 
 			self.download()
 
@@ -29,12 +34,13 @@ class NetgetClient:
 			print("Usage: client.py SERVER_IP_ADDRESS FILENAME")
 
 	def download(self):
+		"""This method will download and save the file."""
 
 		print("[\033[0;32m+\033[0m] Downloading the file...")
 
 		try:
 
-			self.theFile = open("./" + str(sys.argv[2]), 'wb')
+			self.theFile = open("./" + str(sys.argv[2]), 'wb') # this line opens the file that you told the script (binary).
 
 		except:
 
@@ -54,6 +60,7 @@ class NetgetClient:
 		sys.exit(0)
 
 	def ctrlC(self, sig, frame):
+		"""This method can cancel the downloading."""
 
 		ans = input("Do you want to cancel the downloading file? [y,N]> ")
 

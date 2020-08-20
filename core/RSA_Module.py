837c61ecf5cba122e07aa0d97747fb0b6d3bcfea
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-08-20 16:46:15 +0430

    RSAClass added

---
+++ core/RSA_Module.py
@@ -0,0 +1,21 @@
+# License : GPL v3.0
+# Created by Saman Ebrahimnezhad .
+#
+# RSA Cipher Class
+# You can use this script personally but pay attention to the license file.
+
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+from Crypto.Random import new as Random
+from base64 import b64encode, b64decode
+
+class RSAClass:
+	def __init__(self):
+		rng = Random().read
+
+		key = RSA.generate(2048, rng)
+
+        self.__publicKey = key.publickey().exportKey('PEM')
+        self.__privateKey = key.exportKey('PEM')
+
+		return True

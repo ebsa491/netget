fc1d43b6960f7b73346b2d9fc089445b700f2b72
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-08-20 11:47:04 +0430

    ctrlC changed (client.py)

--- core/client.py
+++ core/client.py
@@ -69,6 +69,9 @@ class NetgetClient:
 			# Cancel the downloading
 
 			os.remove("./" + str(sys.argv[2]))
+			self.sock.close()
+			self.theFile.close()
+			sys.exit(1)
 
 if __name__ == '__main__':
 	netget_client = NetgetClient()
